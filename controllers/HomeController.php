8d9ef089a134176fa2e13c56f2dd955545337a11
theartem <TheArtem@yandex.ru>
2014-09-09 22:59:20 +0600

    my first commit

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,9 +2,13 @@
 
 class HomeController extends BaseController {
 
+    public function __construct()
+    {
+        //Asset::add('jquery', 'scripts/core/jquery-2.1.1.js');
+    }
+
 	public function index() {
 		return View::make('home', array('name' => 'World!'));
-		//return "Hello world!";
 	}
 
 }
